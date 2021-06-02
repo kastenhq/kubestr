ce8324577fd92e7626e167b8b0934fdaed9ac5b4
ssuresh <sukhilsuresh@gmail.com>
2021-06-02 15:24:12 -0400

    Handle improper driver names (#72)
    * Update .gitignore
    
    - Ignore Goland/IntelliJ folder
    
    * Update CSI drivers list
    
    * Make CSI Driver list generation more robust
    
    - The script file was unintentionally processing the `Sample Drivers`
      list table and erroring as a result (when scraping data from https://github.com/kubernetes-csi/docs/blob/master/book/src/drivers.md)
    
    * Handle improperly formatted CSI driver name
    
    - This is handled on a case-by-case basis since it is hard to speculate
      on the variations
    - Currently handled for Democratic and Ember CSI
    
    * Restore entries that went missing
    
    * Ensure driver entries are not missed when scraping data

--- scripts/load_csi_provisioners.sh
+++ scripts/load_csi_provisioners.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+CLEANSED_STR=""
+cleanse_str() {
+  case "$1" in
+    "org.democratic-csi.[X]") CLEANSED_STR="org.democratic-csi" ;;
+    "[x].ember-csi.io") CLEANSED_STR="ember-csi.io" ;;
+    *) CLEANSED_STR="$1"
+  esac
+}
+
 current_directory=$(dirname "$0")
 curl https://raw.githubusercontent.com/kubernetes-csi/docs/master/book/src/drivers.md -o ${current_directory}/../extra/csi-drivers
 
@@ -14,10 +28,14 @@ EOT
 
 echo "var CSIDriverList = []*CSIDriver{" >> ${current_directory}/../extra/csi-drivers-temp.go
 while read p; do
-  if [[ $p == [* ]];
-  then
+  if [[ $p == [* ]]; then
     IFS='|'
     read -a fields <<< "$p"
+    if [[ ${#fields[@]} -lt 7 ]]; then
+      echo skipping "${fields[0]}"
+      continue
+    fi
+
     name_url=$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' <<<${fields[0]})
     driver_name=$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' <<<${fields[1]} | sed 's/`//g')
     versions=$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' <<<${fields[2]})
@@ -25,7 +43,14 @@ while read p; do
     persistence=$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' <<<${fields[4]})
     access_modes=$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' <<<${fields[5]}| sed 's/"//g')
     dynamic_provisioning=$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' <<<${fields[6]})
-    features=$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' <<<${fields[7]})
+
+    if [[ ${#fields[@]} -ge 8 ]]; then
+      features=$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' <<<${fields[7]})
+    fi
+
+    cleanse_str "${driver_name}"
+    driver_name="${CLEANSED_STR}"
+
     echo "{NameUrl: \"$name_url\", DriverName: \"$driver_name\", Versions: \"$versions\", Description: \"$description\", Persistence: \"$persistence\", AccessModes: \"$access_modes\", DynamicProvisioning: \"$dynamic_provisioning\", Features: \"$features\"}," >> ${current_directory}/../extra/csi-drivers-temp.go
   fi
 done <${current_directory}/../extra/csi-drivers
