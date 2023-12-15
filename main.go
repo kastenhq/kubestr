cb891526cba218b5c8722594eadf84cbb44be4e6
Denis Voytyuk <5462781+denisvmedia@users.noreply.github.com>
2023-12-15 14:19:49 +0100

    Trigger CI

--- main.go
+++ main.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Imitate changes...
+
 package main
 
 //go:generate ./scripts/load_csi_provisioners.sh
