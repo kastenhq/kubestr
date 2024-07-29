74bd34d09af3c4d6b8f7f011b873801e17c534ad
Shlok Chaudhari <shlok.chaudhari@veeam.com>
2024-07-29 17:28:53 -0500

    Updating the mount paths in CSITestSuite.TestCreateInspectorApplicationForSnapshot

--- pkg/csi/snapshot_inspector_steps_test.go
+++ pkg/csi/snapshot_inspector_steps_test.go
@@ -389,8 +389,8 @@ func (s *CSITestSuite) TestCreateInspectorApplicationForSnapshot(c *C) {
 						GenerateName:   clonedPodGenerateName,
 						PVCName:        "pvc",
 						Namespace:      "ns",
-						ContainerArgs:  []string{"--noauth", "-r", "/data"},
-						MountPath:      "/data",
+						ContainerArgs:  []string{"--noauth", "-r", "/snapshot-data"},
+						MountPath:      "/snapshot-data",
 						RunAsUser:      100,
 						ContainerImage: "filebrowser/filebrowser:v2",
 					}).Return(&v1.Pod{
@@ -444,8 +444,8 @@ func (s *CSITestSuite) TestCreateInspectorApplicationForSnapshot(c *C) {
 						GenerateName:   clonedPodGenerateName,
 						PVCName:        "pvc",
 						Namespace:      "ns",
-						ContainerArgs:  []string{"--noauth", "-r", "/data"},
-						MountPath:      "/data",
+						ContainerArgs:  []string{"--noauth", "-r", "/snapshot-data"},
+						MountPath:      "/snapshot-data",
 						RunAsUser:      100,
 						ContainerImage: "filebrowser/filebrowser:v2",
 					}).Return(&v1.Pod{
