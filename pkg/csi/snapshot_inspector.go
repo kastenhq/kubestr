d6d359bc58803cf8a2e0b24a157c96f25557d13c
Shlok Chaudhari <shlok.chaudhari@veeam.com>
2024-08-08 21:33:06 -0500

    Removing unused snapshotFetchOps from snapshotBrowserSteps

--- pkg/csi/snapshot_inspector.go
+++ pkg/csi/snapshot_inspector.go
@@ -39,10 +39,6 @@ func (r *SnapshotBrowseRunner) RunSnapshotBrowse(ctx context.Context, args *type
 		createAppOps: &applicationCreate{
 			kubeCli: r.KubeCli,
 		},
-		snapshotFetchOps: &snapshotFetch{
-			kubeCli: r.KubeCli,
-			dynCli:  r.DynCli,
-		},
 		portForwardOps: &portforward{},
 		kubeExecutor: &kubeExec{
 			kubeCli: r.KubeCli,
@@ -109,7 +105,6 @@ type SnapshotBrowserStepper interface {
 type snapshotBrowserSteps struct {
 	validateOps          ArgumentValidator
 	versionFetchOps      ApiVersionFetcher
-	snapshotFetchOps     SnapshotFetcher
 	createAppOps         ApplicationCreator
 	portForwardOps       PortForwarder
 	cleanerOps           Cleaner
